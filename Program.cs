77635888486a571da71a6010b127ea18225122c6
rush7777 <rushhardwell@gmail.com>
2023-01-06 02:05:47 +0500

    KESCHA learned to compare

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
-﻿System.Console.WriteLine("Assalomu alaykum xush kelibsiz, Ismingizni kiring: ");
+﻿
+System.Console.WriteLine("Assalomu alaykum xush kelibsiz, Ismingizni kiring: ");
 string name = System.Console.ReadLine();
+
 System.Console.WriteLine($"Endi {name} yoshingizni yozing: ");
 int age = Convert.ToInt32(Console.ReadLine());
+
 int keschasAge = 3;
-System.Console.WriteLine($"Sizning va mening yoshimiz orasidagi tafovut {age - keschasAge} ekan");
+int ageDiff = age - keschasAge;
+
+if(ageDiff > 0){
+  System.Console.WriteLine($"Sizning va mening yoshimiz orasidagi tafovut bor ekan va men sizdan {ageDiff} yoshga kichik ekan man");
+}
+else if(ageDiff == 0){
+  System.Console.WriteLine($"Biz tengqurlar ekanmiz :)");
+}
+else {
+  System.Console.WriteLine($"Sening va mening yoshimiz orasidagi tafovut bor ekan va men sendan {Math.Abs(ageDiff)} yoshga kattaman");
+}
